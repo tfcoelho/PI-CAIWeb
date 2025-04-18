239d090e1d74981cf60980d0006d41b0567b7d66
Oroshibu <43726305+Oroshibu@users.noreply.github.com>
2025-04-18 15:39:42 +0200

    fix action

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,9 @@ module.exports = {
     parser: "@babel/eslint-parser",
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-console": "off",
+    "no-debugger": "off",
+    // "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
 };
